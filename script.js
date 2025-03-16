512fe0f693e7b81f7bb2002bacac94977eb22973
deyvsonfl <deyvsonflp99@gmail.com>
2025-03-16 14:33:25 -0300

    fix

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ document.querySelectorAll("details").forEach((detail) => {
   detail.addEventListener("toggle", () => {
     const icon = detail.querySelector(".icon-img");
     icon.src = detail.open
-      ? "/assets/images/icon-minus.svg"
-      : "/assets/images/icon-plus.svg";
+      ? "./assets/images/icon-minus.svg"
+      : "./assets/images/icon-plus.svg";
   });
 });
